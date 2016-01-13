382e9178ef5969ed4a2c2b0097b1a6135fa085b3
tomgp <tom.g.pearson@gmail.com>
2016-01-13 12:33:53 +0000

    Add a specific small version o the single poll plot

--- visualization-layout.js
+++ visualization-layout.js
@@ -98,12 +98,20 @@ function simpleTimeSeries(width, height, dateDomain, data){
 
 
 function singlePollLayout(width, height, data, metricEmbed){
+    let small = true;
+    if(height>=90) small = false;
+    
 	let margin = {
 			top:20,
 			left:0,
 			bottom:20,
 			right:0
 		};
+        
+    if(small){
+        margin.top = 15;
+        margin.bottom = 0;
+    }
 
 	let scale = d3scale.linear()
 		.domain( [0, data.leave + data.remain + data.undecided] )		//we can't rely on the pollsters numbers to add up to 100 so...
@@ -119,36 +127,38 @@ function singlePollLayout(width, height, data, metricEmbed){
 
 	let config = {
 		title:'',
-		footer:`${data.pollster} polling from ${ftDateFormat(data.startDate)} to ${ftDateFormat(data.endDate)}. ${sampleString}`,
+        small:small,
+		footer:small ? '' : `${data.pollster} polling from ${ftDateFormat(data.startDate)} to ${ftDateFormat(data.endDate)}. ${sampleString}`,
 		margin:margin,
 		width:width,
 		height:height,
 		fontColour:colour.font,
 		metricEmbed: metricEmbed,
-		titleSize:'17',
+		titleSize:'14',
 		titleOffset:{
-			x:0,y:-5
+			x:0,
+            y:small ? -3 : -5
 		},
 		valueLabelSize:Math.min(barHeight, 60),
         valueLabelOffset:{
             x:10,y:-10
         },
 		leave:{
-			title:'Go',
+			title:small ? 'Go - ' + data.leave + '%' : 'Go',
 			width:scale(data.leave),
 			height:barHeight,
 			value:data.leave,
 			fill:colour.leave
 		},
 		remain:{
-			title:'Stay',
+			title:small ? 'Stay - ' + data.remain + '%' : 'Stay',
 			width:scale(data.remain),
 			height:barHeight,
 			value:data.remain,
 			fill:colour.remain
 		},
 		undecided:{
-			title:'¯\\_(ツ)_/¯',
+			title:small ? '' : '¯\\_(ツ)_/¯',
 			width:scale(data.undecided),
 			height:barHeight,
 			value:data.undecided,
